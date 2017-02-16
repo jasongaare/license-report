9c9c0990f6a1e02a56622d68f3c10cbb280aaddc
Jason Gaare <jason.d.gaare@gmail.com>
2017-02-16 10:37:34 -0600

    refactored link to uri and comment to version

--- test/getPackageReportData.test.js
+++ test/getPackageReportData.test.js
@@ -5,12 +5,12 @@ describe('getPackageReportData', function () {
 	this.timeout(10000)
 
 	it('gets the package report data', function (done) {
-		
+
 		getPackageReportData('async', '>0.0.1', function(err, data) {
 			assert.strictEqual(data.name, 'async')
 			assert.strictEqual(data.licenseType, 'MIT')
-			assert.strictEqual(data.link, 'https://github.com/caolan/async.git')
-			
+			assert.strictEqual(data.uri, 'https://github.com/caolan/async.git')
+
 			done()
 		})
 	})
@@ -18,15 +18,15 @@ describe('getPackageReportData', function () {
 	it('returns something when semver is invalid', function (done) {
 		getPackageReportData('async', 'a.b.c', function(err, data) {
 			assert.strictEqual(data.name, 'async')
-			assert.strictEqual(data.comment, 'skipping async (invalid semversion)')
-						
+			assert.strictEqual(data.version, 'skipping async (invalid semversion)')
+
 			done()
 		})
 	})
 	it('returns an error when no versions satisfy the condition', function (done) {
 		getPackageReportData('async', '0.0.1', function(err, data) {
 			assert(err.message.indexOf('cannot find a version that satisfies range') === -1)
-			
+
 			done()
 		})
 	})
